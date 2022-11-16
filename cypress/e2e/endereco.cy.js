3c088823738b615d006b079d6e08d6a2cea87714
Vitor Souza <vitortecam@gmail.com>
2022-11-16 10:38:14 -0400

    Inclução de Page Objects

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,4 +1,5 @@
 ///<reference types="cypress" />
+import EnderecoPage from '../support/page-objects/endereco.page'
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
    beforeEach(() => {
@@ -8,10 +9,10 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     })
     
    });
-    it('Deve fazer cadastro de faturamento com sucesso', () => {
-         //login
-        //cadastro
-           
-
+    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('Cheyenne', 'Lima', 'VGS Consult', 'Brasil', 'Rua Praia Canoa Quebrada', '262', 'Manaus', 'Amazonas', '69041360', '92 984690615', 'cheyenne@vgsconsult.com.br')
+        cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso.')
+        
+        
     });
 });

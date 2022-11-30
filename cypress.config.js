e3dfdfd138506e2ce2f01e3e7b4a76638a82e049
Vitor Souza <vitortecam@gmail.com>
2022-11-30 11:14:54 -0400

    Inclusão de login e conta vinculada ao cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '64io2h',
   e2e: {
     baseUrl: 'http://lojaebac.ebaconline.art.br'
-
+   
   },
-
-  setupNodeEvents(on, config) {
+  
+  setupNodeEvents() {
     // implement node event listeners here
 
   },
